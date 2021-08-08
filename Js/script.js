4fbcf48fffa83b768331c2970f1018df5a06446f
Ishaque-Ahmed <ishaqueahmednasim@gmail.com>
2021-08-09 03:48:00 +0600

    finally final

--- Js/script.js
+++ Js/script.js
@@ -14,26 +14,42 @@ function guesserGame(e) {
 
         while (i < 3) {
 
-                let input = parseInt(prompt("Enter A number (1-10)"));
-
-                if (input === correctAnswer) {
-                        alert("You Win");
+                let input = prompt("Enter A number (1-10)");
+                if (typeof (input) === 'object') {
+                        alert("You Can Always Restart");
                         break;
                 }
-                else if (input < correctAnswer) {
-                        if (i == 2) { alert("you Lose"); }
-                        else {
-                                alert("Correct answer's greater!\nChances Remaining " + chance);
+
+                else if (input !== '') {
+
+                        input = parseInt(input);
+                        console.log(input);
+
+                        if (input === correctAnswer) {
+                                alert("You Win");
+                                break;
                         }
+                        else if (input < correctAnswer) {
+                                if (i == 2) { alert("you Lose"); }
+                                else {
+                                        alert("Correct answer's greater!\nChances Remaining " + chance);
+                                }
+
+                        }
+                        else {
+                                if (i == 2) { alert("you Lose"); }
+                                else {
+                                        alert("Correct answer's smaller\n!Chances Remaing " + chance);
+                                }
+
+                        }
+                } else {
+                        alert("Enter Valid Input");
+                        break;
 
                 }
-                else {
-                        if (i == 2) { alert("you Lose"); }
-                        else {
-                                alert("Correct answer's smaller\n!Chances Remaing " + chance);
-                        }
 
-                }
+
                 i++;
                 chance--;
         }
